9a748f79105b87d762a40f077c6b90627003a017
Derrick <riobad65@gmail.com>
2024-03-19 12:24:07 +0300

    arithmetic test

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 
 // Write your code here
+let num1= 2;
+
+let num2= 31;
+
+const multiply= num1*num2;
+
+console.log(multiply)
+
+//
+let random = Math.floor(Math.random() * 10) + 1;
+    
+//
+let num3 = 64;
+let num4 = 6;
+
+const mod = num3 % num4;
+console.log(mod)
+
+
+//
+let max = Math.max(11,12,13,14,15,16,17,18,19,20);
+    
+ console.log(max)
+
+
+
